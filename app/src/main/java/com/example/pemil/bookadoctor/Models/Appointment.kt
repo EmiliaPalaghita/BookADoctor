c0bc28bf6acc2482c3de00afd5b13963315ddfb7
pemil <p.emilia13@yahoo.com>
2018-10-20 00:03:41 +0300

    added models for User and Appointment

---
+++ app/src/main/java/com/example/pemil/bookadoctor/Models/Appointment.kt
@@ -0,0 +1,8 @@
+package com.example.pemil.bookadoctor.Models
+
+import java.util.*
+
+data class Appointment(val patientId: Int = 0,
+                       val doctorId: Int = 0,
+                       val date: Date = Date(),
+                       val time: String = "HH:mm")
