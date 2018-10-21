989844676ac4a4043e835040204b9b3acaafad45
pemil <p.emilia13@yahoo.com>
2018-10-21 17:58:14 +0300

    added new layout for signup activity

--- app/src/main/java/com/example/pemil/bookadoctor/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/pemil/bookadoctor/Activities/SignUpActivity.kt
@@ -1,11 +1,15 @@
 package com.example.pemil.bookadoctor.Activities
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pemil.bookadoctor.R
 
 class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
 
     }
 }
