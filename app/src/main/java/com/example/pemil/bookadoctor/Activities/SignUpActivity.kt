7c5102b3eed619d84febd9aa7b1732e2539e4c88
ibaciu96 <ionut.b096@yahoo.com>
2018-12-19 21:32:39 +0200

    remove SignUpActivity bullshit

--- app/src/main/java/com/example/pemil/bookadoctor/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/pemil/bookadoctor/Activities/SignUpActivity.kt
@@ -2,9 +2,6 @@ package com.example.pemil.bookadoctor.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import com.example.pemil.bookadoctor.Models.Doctor
-import com.example.pemil.bookadoctor.Models.Patient
 import com.example.pemil.bookadoctor.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -14,18 +11,8 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        input_specialty.visibility = View.GONE
-
         register_button.setOnClickListener {
             registerNewUser()
-        }
-
-        isDoctor.setOnClickListener {
-            if (isDoctor.isPressed) {
-                input_specialty.visibility = View.VISIBLE
-            } else {
-                input_specialty.visibility = View.GONE
-            }
         }
 
     }
@@ -34,14 +21,6 @@ class SignUpActivity : AppCompatActivity() {
 
         val name = input_name.text.toString()
         val email = input_email.text.toString()
-        val isDoctorBool = isDoctor.isPressed
-        if (!isDoctorBool) {
-            val patient = Patient(name = name, email = email)
-        } else {
-            val specialty = input_specialty.text.toString()
-            val address = input_location.text.toString()
-            val doctor = Doctor(name = name, email = email, specialty = specialty, locationAddress = address)
-        }
 
     }
 }
