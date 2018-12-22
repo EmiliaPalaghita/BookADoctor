d4a8c478764594aadff01245b516788242f5a84c
pemil <p.emilia13@yahoo.com>
2018-12-22 02:12:29 +0200

    implemented login with firebase

--- app/src/main/java/com/example/pemil/bookadoctor/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/pemil/bookadoctor/Activities/LoginActivity.kt
@@ -3,12 +3,21 @@ package com.example.pemil.bookadoctor.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.example.pemil.bookadoctor.R
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
@@ -25,5 +34,31 @@ class LoginActivity : AppCompatActivity() {
     private fun loginWithEmail() {
         val email = input_user_email.text.toString()
         val password = input_user_password.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this@LoginActivity, "Login successful", Toast.LENGTH_SHORT).show()
+                updateUI(auth.currentUser)
+            } else {
+                Toast.makeText(this@LoginActivity, "Login failed", Toast.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val firebaseUser = auth.currentUser
+        updateUI(firebaseUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val newIntent = Intent(this, MainActivity::class.java).apply {
+                putExtra("username", user.displayName)
+            }
+            startActivity(newIntent)
+
+        }
     }
 }
