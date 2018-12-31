0885d79c625e620840e4aa7d60507dc510416df2
pemil <p.emilia13@yahoo.com>
2018-12-31 11:52:58 +0200

    implemented the get apppointments from database

--- app/src/main/java/com/example/pemil/bookadoctor/Activities/MainActivity.kt
+++ app/src/main/java/com/example/pemil/bookadoctor/Activities/MainActivity.kt
@@ -4,14 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.example.pemil.bookadoctor.Adapters.AppointmentAdapter
 import com.example.pemil.bookadoctor.Models.Appointment
 import com.example.pemil.bookadoctor.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.main_activity.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appointmentAdapter: AppointmentAdapter
+
+    private var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("appointments")
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var appointmets: MutableList<Appointment> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +37,51 @@ class MainActivity : AppCompatActivity() {
 
         (add_appointment as View).setOnClickListener { openNewAppointmentActivity() }
 
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
+
+        //TODO - create add appointment
+        //TODO - make Date as String (perhaps it is better)
+        //TODO - generate unique ID for each appointment for a patient, so that patient's uid will have a list of id's each representing an appointment
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            retrieveAppointmentsData(user)
+        }
+    }
+
+    private fun retrieveAppointmentsData(user: FirebaseUser) {
+        myRef.child(user.uid).addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Failed to load data", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (data in p0.children) {
+                    val appointment = data.getValue(Appointment::class.java)
+                    if (appointment != null) {
+                        appointmets.add(appointment)
+                    }
+                }
+
+                val sortedList = ArrayList(appointmets)
+                        .sortedWith(compareByDescending<Appointment> { it.date }
+                                .thenBy { it.time })
+
+                appointmets = mutableListOf(*sortedList.toTypedArray())
+
+                appointmentAdapter = AppointmentAdapter(this@MainActivity, appointmets)
+                appointmentsListView.adapter = appointmentAdapter
+            }
+
+        })
     }
 
     private fun openNewAppointmentActivity() {
