3858668f1a1c66d8cd82b178254c82ea3f063dcc
pemil <p.emilia13@yahoo.com>
2019-01-02 21:10:34 +0200

    implemented info button and get all specializations from DB

--- app/src/main/java/com/example/pemil/bookadoctor/Activities/AppointmentActivity.kt
+++ app/src/main/java/com/example/pemil/bookadoctor/Activities/AppointmentActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.pemil.bookadoctor.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.appointment_activity.*
 
 
 class AppointmentActivity : AppCompatActivity() {
+
+    private var database = FirebaseDatabase.getInstance()
+    var specialtiesReference = database.getReference("specialties")
+    var doctorsReference = database.getReference("doctors")
+
+    var specialtiesList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +31,51 @@ class AppointmentActivity : AppCompatActivity() {
         edit_cancel_tv.visibility = View.GONE
 
         back_ib.setOnClickListener { goBackWithoutSaving() }
+
+        save_button.setOnClickListener { saveAppointment() }
+
+        spec_info_ib.setOnClickListener { openSpecializationDetails() }
+
+        retrieveDataFromDB()
+
+    }
+
+    private fun openSpecializationDetails() {
+        val newIntent = Intent(this@AppointmentActivity, SpecializationActivity::class.java)
+        startActivity(newIntent)
+    }
+
+    private fun retrieveDataFromDB() {
+        updateSpecialization()
+
+    }
+
+    private fun updateSpecialization() {
+        if (doctor_name_spinner.selectedItem.toString() != null &&
+                health_center_spinner.selectedItem.toString() != null) {
+            specialtiesReference.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    Toast.makeText(this@AppointmentActivity, "Failed to load data", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    for (data in p0.children) {
+                        val specialtyName = data.child("name").getValue(String::class.java)
+                        specialtiesList.add(specialtyName!!)
+                    }
+
+                    specialization_spinner.adapter = ArrayAdapter<String>(this@AppointmentActivity,
+                            android.R.layout.simple_spinner_item, specialtiesList).apply {
+                        setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    }
+                }
+            })
+        } else {
+            // TODO - implement get from DB with filter
+        }
+    }
+
+    private fun saveAppointment() {
 
     }
 
